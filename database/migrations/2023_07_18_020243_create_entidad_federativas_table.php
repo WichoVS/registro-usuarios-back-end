78609154781f2093de4443c423a751f28b5c423c
Wicho <luis.villanueva.1029@gmail.com>
2023-07-19 21:40:19 -0600

    Controllers and Logic

---
+++ database/migrations/2023_07_18_020243_create_entidad_federativas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entidades_federativas', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre', 128);
+            $table->string('Clave', 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entidades_federativas');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EntidadFederativaController;
+use App\Http\Controllers\EstadoCivilController;
+use App\Http\Controllers\GeneroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post("/login", [UsersController::class, 'login']);
+
+Route::get("/users/", [UsersController::class, 'getAll']);
+Route::get("/users/{curp}", [UsersController::class, 'getUserByCURP']);
+Route::post("/users/", [UsersController::class, 'create']);
+Route::put("/users/", [UsersController::class, 'edit']);
+Route::delete("/users/{curp}", [UsersController::class, 'delete']);
+
+
+Route::get("/entidad-federativa/", [EntidadFederativaController::class, 'getAll']);
+Route::get("/entidad-federativa/{id}", [EntidadFederativaController::class, 'getById']);
+Route::post("/entidad-federativa/", [EntidadFederativaController::class, 'create']);
+Route::put("/entidad-federativa/", [EntidadFederativaController::class, 'edit']);
+Route::delete("/entidad-federativa/{id}", [EntidadFederativaController::class, 'delete']);
+
+Route::get("/estado-civil/", [EstadoCivilController::class, 'getAll']);
+Route::get("/estado-civil/{id}", [EstadoCivilController::class, 'getById']);
+Route::post("/estado-civil/", [EstadoCivilController::class, 'create']);
+Route::put("/estado-civil/", [EstadoCivilController::class, 'edit']);
+Route::delete("/estado-civil/{id}", [EstadoCivilController::class, 'delete']);
+
+Route::get("/genero/", [GeneroController::class, 'getAll']);
+Route::get("/genero/{id}", [GeneroController::class, 'getById']);
+Route::post("/genero/", [GeneroController::class, 'create']);
+Route::put("/genero/", [GeneroController::class, 'edit']);
+Route::delete("/genero/{id}", [GeneroController::class, 'delete']);
